3ab71e9361284482a5cd15f0e94a5cf7109934d2
Jari Duyvejonck <jari.duyvejonck.y0602@student.hogent.be>
2018-12-27 23:41:47 +0100

    add_api_classes

---
+++ Githubclient/Controllers/RepositoryTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RepositoryTableViewController.swift
+//  Githubclient
+//
+//  Created by Jari Duyvejonck on 27/12/2018.
+//  Copyright © 2018 Jari Duyvejonck. All rights reserved.
+//
+
+import Foundation
